f09bafe4a44ee25ca6d5b7aecf1255539085a1f6
nmhung2022 <nmhung2022@gmail.com>
2021-07-22 21:54:25 +0700

    ✅ Add all components and handler 🤡

--- src/components/ControlPanel/index.jsx
+++ src/components/ControlPanel/index.jsx
@@ -3,10 +3,26 @@ import PropTypes from 'prop-types';
 import Session from '../Session/index.jsx';
 import Break from '../Break/index.jsx';
 
-ControlPanel.propTypes = {};
+ControlPanel.propTypes = {
+  started: PropTypes.bool,
+  onStop: PropTypes.func,
+  onStart: PropTypes.func,
+  onReset: PropTypes.func,
+};
 
 function ControlPanel(props) {
-  return <div className="control_panel"></div>;
+  const { started, onStop, onReset, onStart } = props;
+
+  return (
+    <div className="control_panel">
+      <button id="start_stop" onClick={started ? onStop : onStart}>
+        Start/Stop
+      </button>
+      <button onClick={onReset} id="reset">
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default ControlPanel;
